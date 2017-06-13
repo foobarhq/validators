41738a0579dd32bb28d8e86b63225312b8f527f7
Guylian Cox <gc@pictawall.com>
2017-06-13 13:04:31 +0200

    feat: Add support for trimStart & trimEnd to stringValidator.trim

--- __tests__/stringValidator.js
+++ __tests__/stringValidator.js
@@ -22,6 +22,28 @@ describe('stringValidator', () => {
     expect(validate(' hello ? ')).toEqual(' hello ? ');
   });
 
+  test('can trim end', () => {
+    const validate = stringValidator({ trim: 'end' });
+
+    expect(validate(' hello ? ')).toEqual(' hello ?');
+  });
+
+  test('can trim start', () => {
+    const validate = stringValidator({ trim: 'start' });
+
+    expect(validate(' hello ? ')).toEqual('hello ? ');
+  });
+
+  test('can trim custom', () => {
+    const validate = stringValidator({
+      trim(str: string): string {
+        return `${str}11!`;
+      },
+    });
+
+    expect(validate(' hello ? ')).toEqual(' hello ? 11!');
+  });
+
   test('can set a min length', () => {
     const validate = stringValidator({ minLength: 5 });
 
--- src/validators/stringValidator.js
+++ src/validators/stringValidator.js
@@ -5,8 +5,12 @@ import type { Validator } from '../types';
 import optionalValidator from './optionalValidator';
 import type { OptionalArgs } from './optionalValidator';
 
+type TrimFunction = (string) => string;
+
+type TrimType = boolean | TrimFunction | 'start' | 'end' | 'both';
+
 type StringValidatorArgs = {
-  trim?: boolean | Function,
+  trim?: TrimType,
   minLength?: number,
   maxLength?: number,
   ...OptionalArgs,
@@ -23,17 +27,16 @@ export default function stringValidator(options: StringValidatorArgs = defaultSt
     maxLength = Number.POSITIVE_INFINITY,
   } = options;
 
+  // $FlowFixMe
+  const trimmer: ?TrimFunction = trim ? getTrimmer(trim) : null;
+
   function validateString(item: string) {
     if (typeof item !== 'string') {
       throw new InvalidData('Not a string');
     }
 
-    if (trim) {
-      if (typeof trim === 'function') {
-        item = trim(item);
-      } else {
-        item = item.trim();
-      }
+    if (trimmer) {
+      item = trimmer(item);
     }
 
     // $FlowFixMe
@@ -52,4 +55,45 @@ export default function stringValidator(options: StringValidatorArgs = defaultSt
   }
 
   return optionalValidator(validateString, options);
+}
+
+const trimmers = {
+  both: nativeTrim,
+  start: nativeTrimStart,
+  end: nativeTrimEnd,
+};
+
+function getTrimmer(trim: TrimType): ?TrimFunction {
+  const type = typeof trim;
+  if (type === 'function') {
+    // $FlowFixMe
+    return trim;
+  }
+
+  if (trim === true) {
+    return nativeTrim;
+  }
+
+  if (type === 'string') {
+    // $FlowFixMe
+    return trimmers[trim];
+  }
+
+  return null;
+}
+
+function nativeTrim(str) {
+  return str.trim();
+}
+
+// $FlowFixMe
+const trimStart: Function = String.prototype.trimStart || String.prototype.trimLeft;
+function nativeTrimStart(str: string): string {
+  return trimStart.call(str);
+}
+
+// $FlowFixMe
+const trimEnd: Function = String.prototype.trimEnd || String.prototype.trimRight;
+function nativeTrimEnd(str: string): string {
+  return trimEnd.call(str);
 }
